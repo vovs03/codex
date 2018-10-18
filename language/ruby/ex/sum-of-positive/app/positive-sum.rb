d234a78a42e4049359971bbbef0055c0998399df
Vladimir Pavlychev <vladimir.pavlychev@osarts.ru>
2018-10-18 06:37:01 +0300

    Add: variant(broken :warning:) sum + spec

---
+++ language/ruby/ex/sum-of-positive/app/positive-sum.rb
@@ -0,0 +1,18 @@
+# Calculate all positive numbers in array
+
+arr = [34, 56, 7, -8]
+
+def positive_sum(arr)
+  # your code here
+
+  for x in arr
+    arr.sum(arr.each)
+  end
+  @numbers = arr(0..3)
+  @all_items_in_array = arr.first + arr.last
+end
+
+puts(arr.length)
+puts(positive_sum)
+
+# puts(all_items_in_array)
