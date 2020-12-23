54836597c9edbe0e968b69f71bdd90a639478a26
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2020-12-23 15:24:40 +0300

    Add: Step 2: Preparing for extract data from XML | Create WEATHER struct

--- language/golang/weatherscan/scanner.go
+++ language/golang/weatherscan/scanner.go
@@ -5,11 +5,72 @@
 package main
 
 import (
-	"fmt"
+	"encoding/xml"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+// WEATHER https://www.onlinetool.io/xmltogo/
+// https://xml.meteoservice.ru/export/gismeteo/point/7421.xml
+type WEATHER struct {
+	XMLName xml.Name `xml:"MMWEATHER"`
+	Text    string   `xml:",chardata"`
+	REPORT  struct {
+		Text string `xml:",chardata"`
+		Type string `xml:"type,attr"`
+		TOWN struct {
+			Text      string `xml:",chardata"`
+			Index     string `xml:"index,attr"`
+			Sname     string `xml:"sname,attr"`
+			Latitude  string `xml:"latitude,attr"`
+			Longitude string `xml:"longitude,attr"`
+			FORECAST  []struct {
+				Text      string `xml:",chardata"`
+				Day       string `xml:"day,attr"`
+				Month     string `xml:"month,attr"`
+				Year      string `xml:"year,attr"`
+				Hour      string `xml:"hour,attr"`
+				Tod       string `xml:"tod,attr"`
+				Predict   string `xml:"predict,attr"`
+				Weekday   string `xml:"weekday,attr"`
+				PHENOMENA struct {
+					Text          string `xml:",chardata"`
+					Cloudiness    string `xml:"cloudiness,attr"`
+					Precipitation string `xml:"precipitation,attr"`
+					Rpower        string `xml:"rpower,attr"`
+					Spower        string `xml:"spower,attr"`
+				} `xml:"PHENOMENA"`
+				PRESSURE struct {
+					Text string `xml:",chardata"`
+					Max  string `xml:"max,attr"`
+					Min  string `xml:"min,attr"`
+				} `xml:"PRESSURE"`
+				TEMPERATURE struct {
+					Text string `xml:",chardata"`
+					Max  string `xml:"max,attr"`
+					Min  string `xml:"min,attr"`
+				} `xml:"TEMPERATURE"`
+				WIND struct {
+					Text      string `xml:",chardata"`
+					Min       string `xml:"min,attr"`
+					Max       string `xml:"max,attr"`
+					Direction string `xml:"direction,attr"`
+				} `xml:"WIND"`
+				RELWET struct {
+					Text string `xml:",chardata"`
+					Max  string `xml:"max,attr"`
+					Min  string `xml:"min,attr"`
+				} `xml:"RELWET"`
+				HEAT struct {
+					Text string `xml:",chardata"`
+					Min  string `xml:"min,attr"`
+					Max  string `xml:"max,attr"`
+				} `xml:"HEAT"`
+			} `xml:"FORECAST"`
+		} `xml:"TOWN"`
+	} `xml:"REPORT"`
+}
 
 func main() {
 	// https://golang.org/pkg/net/http/#Response
@@ -27,5 +88,10 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(string(byteValue))
+	//fmt.Println(string(byteValue))
+	var weather WEATHER
+	err = xml.Unmarshal(byteValue, &weather)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
