1bea59e34716a6ad0cc3b57ec78750af0bb823c3
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2021-05-01 23:20:23 +0300

    Add: easy RAM cleaner for currently OSX processes | RAMfix  | bash

---
+++ osx/scripts/ramfix/ramfix_env.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+AUTHOR="Vladimir Pavlychev @vovs03 vladimir.keyjoo@gmail.com"
+# Date: 2021-05-01
+# file ramfix_env.py
+# Path: ~/code/github/codex/osx/scripts/ramfix
+
+APP_VER="0.0.1"
+APP_NAME="RAMfix"
+
+print("App :", APP_NAME)
+print("App version:", APP_VER)
