11a97e1a68c87ba114fc228317563fcac5de15c5
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2021-07-07 19:31:42 +0300

    Add: .gitignore | go-in-practice-page30

--- language/golang/go-in-practice/p30/one.go
+++ language/golang/go-in-practice/p30/one.go
@@ -13,11 +13,6 @@ func main() {
 
 	for i := 1; i < 6; i++ {
 		c := &i
-		//fmt.Println(i)
-
-		//fname := `f.txt` имя файла чтобы к нему подключить счётчик
-		//cFile, _ := os.Create(fname+i, _)
-		//cFile, _ := os.Create(`f.txt`)
 
 		FileName := "f" + strconv.Itoa(i) + ".txt"
 		cFile, _ := os.Create(FileName)
@@ -25,14 +20,9 @@ func main() {
 		fmt.Println(i, "file was created:", c, *c)
 		defer cFile.Close()
 
-		// прерывание счётчика на 1 милисекунду * N
-		time.Sleep(time.Millisecond * 10)
-
 		// проверка удаления файла - таймер для отложенности
-		time.Sleep(time.Millisecond * 800)
-		// удаление файла с указанным именем
-		//defer os.Remove(`f.txt`)
-		defer os.Remove(FileName)
+		time.Sleep(time.Millisecond * 100)
+		//defer os.Remove(FileName)
 	}
 
 	fmt.Println("count was done")
