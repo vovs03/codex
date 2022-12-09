d0632ba7a1a711a13c1514a50d80deb5f4e5348f
Vladimir Victor Pavlychev <vladimir.keyjoo@gmail.com>
2022-12-09 13:18:48 +0300

    Add: Clean all comments + len n = 4

--- language/python/basic/square_sum.py
+++ language/python/basic/square_sum.py
@@ -1,18 +1,9 @@
-numbers = [1,2,3]
+numbers = [1,2,3,5]
 
 def square_sum(numbers):
   sx = []
-  # print("Тип sx:", type(sx))
-  # sx.append(3)
-  # print("Тип sx:", type(sx))
-  # print(sx)
-
   for i in numbers:
-    # print(i)
     sx.append(i*i)
-    # print(sx)
-  
-  # return print(sum(numbers))
   print(sum(sx))
-
+  
 square_sum(numbers)
