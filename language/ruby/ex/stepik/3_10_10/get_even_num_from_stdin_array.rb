881895070dc75a9d6a53d4bec4273c073af0326a
Vladimir Victor Pavlychev <vladimir.keyjoo@gmail.com>
2023-11-05 09:42:28 +0300

    Refactor: 💎delete a,b variables

--- language/ruby/ex/stepik/3_10_10/get_even_num_from_stdin_array.rb
+++ language/ruby/ex/stepik/3_10_10/get_even_num_from_stdin_array.rb
@@ -1,9 +1,5 @@
-# gets two nums
-a = gets.to_i
-b = gets.to_i
-
-# Create array
-arr = (a..b).to_a
+# Create array from stdin(gets two nums)
+arr = (gets.to_i..gets.to_i).to_a
 
 #выборка по чётности числа и вставка пробела
 # Use this case: https://stackoverflow.com/questions/33284487/print-only-even-numbers-from-an-array
