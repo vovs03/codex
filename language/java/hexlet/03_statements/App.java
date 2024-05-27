27218a6af283ad0c73356a76d9e57a9e372c3c11
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2024-05-27 15:46:58 +0300

    Add: 🦄 #57 printNames

---
+++ language/java/hexlet/03_statements/App.java
@@ -0,0 +1,7 @@
+class App {
+    public static void main(String[] args) {
+        System.out.println("Robert");
+        System.out.println("Stannis");
+        System.out.println("Renly");
+    }
+}
