581e2b5bcf90b636fb0d97dc23468eb041fb8d69
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2024-05-28 15:03:40 +0300

    Add: #61 ⁉️  Output result math ops

---
+++ language/java/hexlet/04_arithmetics/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println(-8 / -4);
+        System.out.println(100 % 3);
+        System.out.println((-8 / -4) + (100 % 3));
+    }
+}
