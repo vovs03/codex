4fa8068585c37d61f738faa4f1ff06f44e92ef17
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2024-06-07 18:12:01 +0300

    Add: #66 🐱 Kotlin

---
+++ language/kotlin/hello.kt
@@ -0,0 +1,12 @@
+// 2024-06-07 17:35
+// https://ru.stackoverflow.com/questions/1083944/Запуск-kotlin-кода-без-ide
+
+fun main(args: Array<String>) {
+	println("HEllo, Kotlin!")
+}
+
+// 2. Compiling:
+// `$ kotlinc hello.kt -include-runtime -d hello.jar`
+
+// 3. Run:
+// `$ java -jar hello.jar`
