f4a7db0aa468e2cd424bf93da1a84c374f523792
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2024-06-24 17:11:00 +0300

    Add:  🦅 Solution for chang Ternary Equal(5) | JR_3.7_0310

--- language/java/src/ChangeTernary.java
+++ language/java/src/ChangeTernary.java
@@ -15,7 +15,15 @@ public class ChangeTernary {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        String result = number < 5 ? "число меньше 5" : number > 5 ? "число больше 5" : "число равно 5";
+
+        String result = "";
+        if (number < 5) {
+            result = "число меньше 5";
+        } else if (number > 5) {
+            result = "число больше 5";
+        } else if (number == 5) {
+            result = "число равно 5";
+        }
         System.out.println(result);
     }
 }
