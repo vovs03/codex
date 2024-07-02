e439bd39b9bcb9bd478aaaf1a586fed614c2b57f
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2024-07-02 10:29:20 +0300

    Add: it is important to maintain order by use vars

---
+++ language/java/src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int number = 10; // тест не принимал задачу пока не был чётко простроен порядок:
+        String text = "Hello, World!"; // инициализация - number, text, letter
+        char letter = 'A';
+        System.out.println("Number is: " + number);// Вывод, соответственно в том же порядке!
+        System.out.println("Text is: " + text);
+        System.out.println("Letter is: " + letter);
+
+    }
+}
